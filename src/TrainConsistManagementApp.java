724417f08fe41c0c5e34b730f4e7017cacc9ab9a
vamshinelluri7-dotcom <vamshinelluri7@gmail.com>
2026-03-25 15:04:20 +0530

    UC4: Maintain Ordered Bogie IDs (TreeSet & SortedSet)

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,19 +1,27 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.LinkedList;
 
 public class TrainConsistManagementApp {
 
     public static void main(String[] args) {
         System.out.println("=== Train Consist Management App ===");
 
-        Set<String> bogieIds = new HashSet<>();
+        LinkedList<String> trainConsist = new LinkedList<>();
 
-        bogieIds.add("BG101");
-        bogieIds.add("BG102");
-        bogieIds.add("BG103");
-        bogieIds.add("BG101");
+        trainConsist.add("Engine");
+        trainConsist.add("Sleeper");
+        trainConsist.add("AC");
+        trainConsist.add("Cargo");
+        trainConsist.add("Guard");
 
-        System.out.println("Unique Bogie IDs in System: " + bogieIds);
-        System.out.println("Total Unique Bogies: " + bogieIds.size());
+        System.out.println("Initial Train Consist: " + trainConsist);
+
+        trainConsist.add(2, "Pantry Car");
+        System.out.println("After inserting Pantry Car at position 2: " + trainConsist);
+
+        trainConsist.removeFirst();
+        trainConsist.removeLast();
+
+        System.out.println("Final Ordered Consist: " + trainConsist);
+        System.out.println("Total Bogies: " + trainConsist.size());
     }
 }
